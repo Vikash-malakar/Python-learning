d66318d78b541a0867b35fea685e49dd626503b6
Vikash Malakar <vikashmalakar312@gmail.com>
2026-04-11 08:42:13 +0530

    build logic python

--- update.py
+++ update.py
@@ -1,31 +1,14 @@
-print("=== Welcome to ATM  ===")
+print("=== Number Pyramid  ===")
 
-balance = 1000  # Initial balance
+rows = int(input("Enter number of rows: "))
 
-print("1. Check Balance")
-print("2. Deposit")
-print("3. Withdraw")
+for i in range(1, rows + 1):
+    # spaces
+    for j in range(rows - i):
+        print(" ", end="")
 
-choice = int(input("Enter your choice (1-3): "))
+    # numbers
+    for k in range(1, i + 1):
+        print(k, end=" ")
 
-if choice == 1:
-    print(" Your Balance is:", balance)
-
-elif choice == 2:
-    amount = float(input("Enter amount to deposit: "))
-    balance += amount
-    print(" Deposited Successfully!")
-    print(" New Balance:", balance)
-
-elif choice == 3:
-    amount = float(input("Enter amount to withdraw: "))
-    
-    if amount > balance:
-        print(" Insufficient Balance!")
-    else:
-        balance -= amount
-        print(" Withdrawal Successful!")
-        print(" Remaining Balance:", balance)
-
-else:
-    print(" Invalid Choice")
+    print()

35d72a4afb8b2c1dd6373da7c7e7ecaaeacb05ca
Vikash Malakar <vikashmalakar312@gmail.com>
2026-04-12 17:30:37 +0530

    build logic python

--- update.py
+++ update.py
@@ -1,14 +1,30 @@
-print("=== Number Pyramid  ===")
+print("=== To-Do List Manager 📋 ===")
 
-rows = int(input("Enter number of rows: "))
+tasks = []
 
-for i in range(1, rows + 1):
-    # spaces
-    for j in range(rows - i):
-        print(" ", end="")
+while True:
+    print("\n1. Add Task")
+    print("2. Show Tasks")
+    print("3. Exit")
 
-    # numbers
-    for k in range(1, i + 1):
-        print(k, end=" ")
+    choice = input("Enter your choice: ")
 
-    print()
+    if choice == "1":
+        task = input("Enter your task: ")
+        tasks.append(task)
+        print(" Task Added!")
+
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("📭 No tasks yet!")
+        else:
+            print("\n Your Tasks:")
+            for i in range(len(tasks)):
+                print(i + 1, "-", tasks[i])
+
+    elif choice == "3":
+        print("👋 Exiting... Bye!")
+        break
+
+    else:
+        print(" Invalid Choice")

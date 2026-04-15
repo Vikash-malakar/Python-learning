6aed9aaff6add30d2e572b3349782dc05257bece
Vikash Malakar <vikashmalakar312@gmail.com>
2026-04-15 20:03:19 +0530

    react learnig

--- update.py
+++ update.py
@@ -1,22 +1,42 @@
-print("=== Password Strength Checker ===")
+print("=== Expense Tracker ===")
 
-password = input("Enter your password: ")
+expenses = []
 
-length = len(password)
+while True:
+    print("\n1. Add Expense")
+    print("2. Show Expenses")
+    print("3. Show Total")
+    print("4. Show Highest Expense")
+    print("5. Exit")
 
-has_digit = False
-has_upper = False
+    choice = input("Enter choice: ")
 
-for ch in password:
-    if ch.isdigit():
-        has_digit = True
-    if ch.isupper():
-        has_upper = True
+    if choice == "1":
+        amount = float(input("Enter expense amount: "))
+        expenses.append(amount)
+        print("Expense Added")
 
-# Logic
-if length >= 8 and has_digit and has_upper:
-    print(" Strong Password")
-elif length >= 6:
-    print(" Medium Password")
-else:
-    print(" Weak Password")
+    elif choice == "2":
+        if len(expenses) == 0:
+            print("No expenses found")
+        else:
+            print("Expenses List:")
+            for i in range(len(expenses)):
+                print(i + 1, "=", expenses[i])
+
+    elif choice == "3":
+        total = sum(expenses)
+        print("Total Expense:", total)
+
+    elif choice == "4":
+        if len(expenses) == 0:
+            print("No expenses found")
+        else:
+            print("Highest Expense:", max(expenses))
+
+    elif choice == "5":
+        print("Program Closed")
+        break
+
+    else:
+        print("Invalid Choice")

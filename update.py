83aa010f54746d6e776719f9ebed4dbc7cad40da
Vikash Malakar <vikashmalakar312@gmail.com>
2026-04-16 20:53:56 +0530

    build logic python

--- update.py
+++ update.py
@@ -1,42 +1,36 @@
-print("=== Expense Tracker ===")
 
-expenses = []
+print("=== Student Result System ===")
 
-while True:
-    print("\n1. Add Expense")
-    print("2. Show Expenses")
-    print("3. Show Total")
-    print("4. Show Highest Expense")
-    print("5. Exit")
+name = input("Enter student name: ")
 
-    choice = input("Enter choice: ")
+sub1 = float(input("Enter marks of Subject 1: "))
+sub2 = float(input("Enter marks of Subject 2: "))
+sub3 = float(input("Enter marks of Subject 3: "))
+sub4 = float(input("Enter marks of Subject 4: "))
+sub5 = float(input("Enter marks of Subject 5: "))
 
-    if choice == "1":
-        amount = float(input("Enter expense amount: "))
-        expenses.append(amount)
-        print("Expense Added")
+total = sub1 + sub2 + sub3 + sub4 + sub5
+percentage = total / 5
 
-    elif choice == "2":
-        if len(expenses) == 0:
-            print("No expenses found")
-        else:
-            print("Expenses List:")
-            for i in range(len(expenses)):
-                print(i + 1, "=", expenses[i])
+if percentage >= 90:
+    grade = "A+"
+elif percentage >= 75:
+    grade = "A"
+elif percentage >= 60:
+    grade = "B"
+elif percentage >= 40:
+    grade = "C"
+else:
+    grade = "Fail"
 
-    elif choice == "3":
-        total = sum(expenses)
-        print("Total Expense:", total)
+if percentage >= 40:
+    result = "Pass"
+else:
+    result = "Fail"
 
-    elif choice == "4":
-        if len(expenses) == 0:
-            print("No expenses found")
-        else:
-            print("Highest Expense:", max(expenses))
-
-    elif choice == "5":
-        print("Program Closed")
-        break
-
-    else:
-        print("Invalid Choice")
+print("\n=== Result ===")
+print("Name:", name)
+print("Total Marks:", total)
+print("Percentage:", percentage)
+print("Grade:", grade)
+print("Status:", result)

057159f5447b1432c22cf2f2101dcc004f29630c
Vikash Malakar <vikashmalakar312@gmail.com>
2026-04-17 09:52:28 +0530

    build logic python

--- update.py
+++ update.py
@@ -1,36 +1,21 @@
+import random
 
-print("=== Student Result System ===")
+print("=== Number Guessing Game ===")
 
-name = input("Enter student name: ")
+secret_number = random.randint(1, 20)
+attempt = 0
 
-sub1 = float(input("Enter marks of Subject 1: "))
-sub2 = float(input("Enter marks of Subject 2: "))
-sub3 = float(input("Enter marks of Subject 3: "))
-sub4 = float(input("Enter marks of Subject 4: "))
-sub5 = float(input("Enter marks of Subject 5: "))
+while True:
+    guess = int(input("Enter your guess (1 to 20): "))
+    attempt += 1
 
-total = sub1 + sub2 + sub3 + sub4 + sub5
-percentage = total / 5
+    if guess == secret_number:
+        print("Correct Guess")
+        print("Total Attempts:", attempt)
+        break
 
-if percentage >= 90:
-    grade = "A+"
-elif percentage >= 75:
-    grade = "A"
-elif percentage >= 60:
-    grade = "B"
-elif percentage >= 40:
-    grade = "C"
-else:
-    grade = "Fail"
+    elif guess > secret_number:
+        print("Too High")
 
-if percentage >= 40:
-    result = "Pass"
-else:
-    result = "Fail"
-
-print("\n=== Result ===")
-print("Name:", name)
-print("Total Marks:", total)
-print("Percentage:", percentage)
-print("Grade:", grade)
-print("Status:", result)
+    else:
+        print("Too Low")

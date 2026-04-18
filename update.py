dce49b623f0bb4ad6ed3a5da5cd25680f91970f6
Vikash Malakar <vikashmalakar312@gmail.com>
2026-04-18 10:28:21 +0530

    build logic python

--- update.py
+++ update.py
@@ -1,21 +1,43 @@
-import random
+print("=== ATM Machine ===")
 
-print("=== Number Guessing Game ===")
+pin = "1234"
+balance = 5000
 
-secret_number = random.randint(1, 20)
-attempt = 0
+user_pin = input("Enter PIN: ")
 
-while True:
-    guess = int(input("Enter your guess (1 to 20): "))
-    attempt += 1
+if user_pin == pin:
 
-    if guess == secret_number:
-        print("Correct Guess")
-        print("Total Attempts:", attempt)
-        break
+    while True:
+        print("\n1. Check Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
 
-    elif guess > secret_number:
-        print("Too High")
+        choice = input("Enter choice: ")
 
-    else:
-        print("Too Low")
+        if choice == "1":
+            print("Current Balance:", balance)
+
+        elif choice == "2":
+            amount = float(input("Enter deposit amount: "))
+            balance += amount
+            print("Updated Balance:", balance)
+
+        elif choice == "3":
+            amount = float(input("Enter withdraw amount: "))
+
+            if amount <= balance:
+                balance -= amount
+                print("Updated Balance:", balance)
+            else:
+                print("Insufficient Balance")
+
+        elif choice == "4":
+            print("Thank You")
+            break
+
+        else:
+            print("Invalid Choice")
+
+else:
+    print("Wrong PIN")

116aef1a8cd92793a8b8bb964ee0382014adb608
Vikash Malakar <vikashmalakar312@gmail.com>
2026-04-25 22:53:24 +0530

    build logic python

--- update.py
+++ update.py
@@ -1,44 +1,15 @@
-print("=== Voting System ===")
+print("=== Employee Salary System ===")
 
-votes = {
-    "Rahul": 0,
-    "Amit": 0,
-    "Vikash": 0
-}
+name = input("Enter employee name: ")
+basic_salary = float(input("Enter basic salary: "))
 
-while True:
-    print("\n1. Cast Vote")
-    print("2. Show Results")
-    print("3. Show Winner")
-    print("4. Exit")
+bonus = basic_salary * 0.10
+tax = basic_salary * 0.05
+final_salary = basic_salary + bonus - tax
 
-    choice = input("Enter choice: ")
-
-    if choice == "1":
-        print("\nCandidates:")
-        for name in votes:
-            print(name)
-
-        candidate = input("Enter candidate name: ")
-
-        if candidate in votes:
-            votes[candidate] += 1
-            print("Vote Casted Successfully")
-        else:
-            print("Invalid Candidate")
-
-    elif choice == "2":
-        print("\nVoting Results:")
-        for name, count in votes.items():
-            print(name, ":", count)
-
-    elif choice == "3":
-        winner = max(votes, key=votes.get)
-        print("Winner is:", winner)
-
-    elif choice == "4":
-        print("Program Closed")
-        break
-
-    else:
-        print("Invalid Choice")
+print("\n=== Salary Slip ===")
+print("Employee Name:", name)
+print("Basic Salary:", basic_salary)
+print("Bonus (10%):", bonus)
+print("Tax (5%):", tax)
+print("Final Salary:", final_salary)

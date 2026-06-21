5091fae45a2b84fe6673c92ed019e12e036425dd
Vikash Malakar <vikashmalakar312@gmail.com>
2026-06-21 20:53:38 +0530

    learn python

---
+++ task.py
@@ -0,0 +1,13 @@
+text = input("Enter a sentence: ").lower()
+
+frequency = {}
+
+for ch in text:
+    if ch != " ":
+        if ch in frequency:
+            frequency[ch] += 1
+        else:
+            frequency[ch] = 1
+
+for ch, count in frequency.items():
+    print(ch, ":", count)

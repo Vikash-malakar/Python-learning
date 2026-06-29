90b5da523879d93f9d3a276d31406bb9df50d825
Vikash Malakar <vikashmalakar312@gmail.com>
2026-06-29 10:00:00 +0530

    learn_python

--- code.py
+++ code.py
@@ -1,17 +1,12 @@
 number = int(input("Enter a number: "))
 
-is_prime = True
+total = 0
 
-if number <= 1:
-    is_prime = False
+for i in range(1, number):
+    if number % i == 0:
+        total += i
 
+if total == number:
+    print(number, "is a Perfect Number")
 else:
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-            break
-
-if is_prime:
-    print(number, "is a Prime Number")
-else:
-    print(number, "is not a Prime Number")
+    print(number, "is not a Perfect Number")
